8ded76c48925caf2ad1606f0a52820edce216604
Andrés Lucas Enciso <andresin87@gmail.com>
2016-06-21 21:16:24 +0200

    ADD icon component stories.

---
+++ components/material-ui/stories/icon.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import AlarmIcon from 'react-material-icons/icons/action/alarm';
+import GiftCard from 'react-material-icons/icons/action/card-giftcard';
+import Dashboard from 'react-material-icons/icons/action/dashboard';
+import Backup from 'react-material-icons/icons/action/backup';
+import {blue500, red500, greenA200} from 'material-ui/styles/colors';
+
+import { storiesOf, action } from '@kadira/storybook';
+
+storiesOf('Icon', module)
+.add('AlarmIcon', () => (
+  <div>
+    <MuiThemeProvider
+      muiTheme={getMuiTheme({})}
+    >
+      <AlarmIcon color={blue500}/>
+    </MuiThemeProvider>
+  </div>
+))
+.add('Dashboard', () => (
+  <div>
+    <MuiThemeProvider
+      muiTheme={getMuiTheme({})}
+    >
+      <Dashboard color={red500}/>
+    </MuiThemeProvider>
+  </div>
+))
+.add('GiftCard', () => (
+  <div>
+    <MuiThemeProvider
+      muiTheme={getMuiTheme({})}
+    >
+      <GiftCard color={greenA200}/>
+    </MuiThemeProvider>
+  </div>
+))
+.add('Backup', () => (
+  <div>
+    <MuiThemeProvider
+      muiTheme={getMuiTheme({})}
+    >
+      <Backup color={greenA200}/>
+    </MuiThemeProvider>
+  </div>
+));
