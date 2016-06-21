0d85cd504fefd1ee7365ffe88ce7d1622a4f1a5c
Andrés Lucas Enciso <andresin87@gmail.com>
2016-06-21 22:50:53 +0200

    ADD new story to the badge material-ui component

--- components/material-ui/stories/badge.js
+++ components/material-ui/stories/badge.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Badge from 'material-ui/Badge';
 import IconButton from 'material-ui/IconButton';
 import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
+import UploadIcon from 'material-ui/svg-icons/file/cloud-upload';
+import FolderIcon from 'material-ui/svg-icons/file/folder-open';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -29,6 +31,27 @@ storiesOf('Badge', module)
           <IconButton tooltip="Notifications">
             <NotificationsIcon />
           </IconButton>
+        </Badge>
+      </div>
+    </MuiThemeProvider>
+  </div>
+))
+.add('Further Examples', () => (
+  <div>
+    <MuiThemeProvider
+      muiTheme={getMuiTheme({})}
+    >
+      <div>
+        <Badge
+          badgeContent={<IconButton tooltip="Backup"><UploadIcon /></IconButton>}
+        >
+          <FolderIcon />
+        </Badge>
+        <Badge
+          badgeContent="&copy;"
+          badgeStyle={{fontSize: 20}}
+        >
+          Company Name
         </Badge>
       </div>
     </MuiThemeProvider>
