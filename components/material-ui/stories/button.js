99505b771aa53267d2f5b82dc097aeeb922493b2
Andrés Lucas Enciso <andresin87@gmail.com>
2016-06-23 00:23:26 +0200

    ADD material-ui raised button story.

--- components/material-ui/stories/button.js
+++ components/material-ui/stories/button.js
@@ -6,6 +6,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { storiesOf, action } from '@kadira/storybook';
 
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import FontIcon from 'material-ui/FontIcon';
 import ActionAndroid from 'material-ui/svg-icons/action/android'
@@ -20,12 +21,12 @@ const styles = {
     right: 0,
     left: 0,
     width: '100%',
-    opacity: 0,
-  },
+    opacity: 0
+  }
 };
 
 storiesOf('Button', module)
-.add('Flat Button',() => (
+.add('Flat Button', () => (
   <div>
     <MuiThemeProvider
       muiTheme={getMuiTheme({})}
@@ -75,6 +76,61 @@ storiesOf('Button', module)
           secondary={true}
           icon={<FontIcon className="muidocs-icon-custom-github" />}
           style={ {margin: 12} }
+        />
+      </div>
+    </MuiThemeProvider>
+  </div>
+))
+.add('Raised Button', () => (
+  <div>
+    <MuiThemeProvider
+      muiTheme={getMuiTheme({})}
+    >
+      <div>
+        <h1>Simple Examples</h1>
+        <RaisedButton label="Default" style={ {margin: 12} } />
+        <RaisedButton label="Primary" primary={true} style={ {margin: 12} } />
+        <RaisedButton label="Secondary" secondary={true} style={ {margin: 12} } />
+        <RaisedButton label="Disabled" disabled={true} style={ {margin: 12} } />
+        <h1>Complex Examples</h1>
+        <RaisedButton
+          label="Choose an Image"
+          labelPosition="before"
+          style={{margin: 12}}
+        >
+          <input type="file" style={styles.exampleImageInput} />
+        </RaisedButton>
+        <RaisedButton
+          label="Label before"
+          labelPosition="before"
+          primary={true}
+          icon={<ActionAndroid />}
+          style={{margin: 12}}
+        />
+        <RaisedButton
+          label="Github Link"
+          linkButton={true}
+          href="https://github.com/callemall/material-ui"
+          secondary={true}
+          style={{margin: 12}}
+          icon={<FontIcon className="muidocs-icon-custom-github" />}
+        />
+        <h1>Icon Examples</h1>
+        <RaisedButton
+          icon={<ActionAndroid />}
+          style={{margin: 12}}
+        />
+        <RaisedButton
+          backgroundColor="#a4c639"
+          icon={<ActionAndroid color={fullWhite} />}
+          style={{margin: 12}}
+        />
+        <RaisedButton
+          linkButton={true}
+          href="https://github.com/callemall/material-ui"
+          secondary={true}
+          icon={<FontIcon className="muidocs-icon-custom-github" />}
+          style={{margin: 12}}
         />
       </div>
     </MuiThemeProvider>
